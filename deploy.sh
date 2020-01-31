02b38b46600381c02906fc9d2d1d4beaa1879610
Phumelele Msimang <phumelele.msimang@wesbank.co.za>
2020-01-31 11:46:35 +0200

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build - t thabizolo/multi-client:latest -t thabizolo/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build  -t thabizolo/multi-server:latest -t thabizolo/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build  -t thabizolo/multi-worker:latest -t thabizolo/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push thabizolo/multi-client:latest
+docker push thabizolo/multi-server:latest
+docker push thabizolo/multi-worker:latest
+
+docker push thabizolo/multi-client:$SHA
+docker push thabizolo/multi-server:$SHA
+docker push thabizolo/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=thabizolo/multi-server:$SHA
+kubectl set image deployments/client-deployment client=thabizolo/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=thabizolo/multi-worker:$SHA
